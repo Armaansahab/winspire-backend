ab049cb1f98a36fd36da118fd9a38f2c4b7b6670
Armaansahab <asyedarmaan345@gmail.com>
2025-07-30 21:26:11 +0530

    origin for socket io allowed all

--- server.js
+++ server.js
@@ -14,13 +14,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://127.0.0.1:5173",
-      "http://127.0.0.1:5174"
-    ],
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true
   }
