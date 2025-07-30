c7f144c3537e3ea26fdf6bc540242b51833d7b97
Armaansahab <asyedarmaan345@gmail.com>
2025-07-30 21:25:11 +0530

    origins allowed all

--- server.js
+++ server.js
@@ -27,13 +27,7 @@ const io = socketIo(server, {
 });
 
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174"
-  ],
+  origin: "*",
   credentials: true
 }));
 
